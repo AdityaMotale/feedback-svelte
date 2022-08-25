cb42aece0beee181bd724a4f55a8f135040e5ba3
Aditya <77727680+AdityaMotale@users.noreply.github.com>
2022-08-25 20:49:52 +0530

    init

---
+++ src/components/CounterRounded.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  let count: number = 0;
+
+  const incr = () => {
+    count += 1;
+  };
+</script>
+
+<div class="rounded-full bg-purple-900 h-28 w-28" on:click={incr}> 
+    {count}
+</div>
